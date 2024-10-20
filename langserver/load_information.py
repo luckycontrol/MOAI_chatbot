38200d517decdd27f9f8bdc0e445ab9a595a2698
luckycontrol <beansu1@naver.com>
2024-10-20 21:30:37 +0900

    MOAI chatbot

--- langserver/load_information.py
+++ langserver/load_information.py
@@ -1,31 +1,61 @@
 import chromadb
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import TextLoader, JSONLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
 import json
+import os
 
+def format_docs(docs):
+    formatted_docs = []
+    for doc in docs:
+        content = doc.page_content
+        metadata = doc.metadata
+        formatted_doc = f"컨텐츠:\n{content}\n\n메타데이터:\n"
+        for key, value in metadata.items():
+            formatted_doc += f"{key}: {value}\n"
+        formatted_docs.append(formatted_doc)
+    return '\n\n---\n\n'.join(formatted_docs)
+
+def metadata_func(record: dict, metadata: dict) -> dict:
+    metadata.update(record.get("metadata", {}))
+    for key, value in metadata.items():
+        if isinstance(value, list):
+            metadata[key] = ', '.join(map(str, value))
+    return metadata
+
+# example 데이터 로드
+examples = json.load(open(os.getcwd() + "/data/platform_few_shot.json", "r", encoding="utf-8"))
+
+# 임베딩 모델 로드
 embeddings = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-m3",
-    model_kwargs={"device": "cuda"},
+    model_name="paraphrase-multilingual-mpnet-base-v2",
+    model_kwargs={"device": "mps"},
     encode_kwargs={"normalize_embeddings": True}
 )
 
-loader = TextLoader(r"D:\projects\MOAI_chatbot\data\platform_information.txt")
+# RAG 데이터 로드
+# loader = TextLoader(os.getcwd() + "/data/platform_information.txt")
+loader = JSONLoader(
+    file_path=os.getcwd() + "/data/platform_information_rag.json",
+    jq_schema='.[]',
+    content_key='content',
+    metadata_func=metadata_func
+)
 pages = loader.load()
-
-examples = json.load(open(r"D:\projects\MOAI_chatbot\data\platform_few_shot.json", "r", encoding="utf-8"))
 
 text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=500,
-    chunk_overlap=50
+    chunk_overlap=150
 )
 
-docs = text_splitter.split_documents(pages)
+# docs = text_splitter.split_documents(pages)
 
-chroma_client = chromadb.PersistentClient(path="D:\projects\MOAI_chatbot\chroma_data")
+chroma_client = chromadb.PersistentClient(path=os.getcwd() + "/chroma_data")
 
-vectorstore = Chroma.from_documents(docs, embeddings, client=chroma_client)
+vectorstore = Chroma.from_documents(pages, embeddings, client=chroma_client)
 retriever = vectorstore.as_retriever(search_kwargs={"k": 1})
 
-# print(retriever.invoke("Master 와 Slave 가 연결되었다면 어떻게 표시돼?"))
+# result = retriever.invoke("PLC 와 연결된 상태를 확인하는 방법을 알려줘.")
+# formatted_result = format_docs(result)
+# print(formatted_result)
--- langserver/prompt.py
+++ langserver/prompt.py
@@ -3,12 +3,12 @@ import sys
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from langchain_core.prompts import ChatPromptTemplate, FewShotPromptTemplate, PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, FewShotPromptTemplate, PromptTemplate, PipelinePromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 from langserver.load_model import model
-from langserver.load_information import retriever, examples
+from langserver.load_information import retriever, examples, format_docs
 
 example_prompt = PromptTemplate.from_template(
     "question: {question}\nanswer: {answer}"
@@ -17,25 +17,34 @@ example_prompt = PromptTemplate.from_template(
 few_shot_prompt = FewShotPromptTemplate(
     examples=examples,
     example_prompt=example_prompt,
-    prefix="다음은 질문과 답변의 예시입니다:",
-    suffix="이제 다음 질문에 답해주세요:\n질문: {question}\n답변:",
+    prefix='다음은 질문과 답변의 예시입니다.',
+    suffix='''
+    <example>   
+    {few_shot_examples}
+    </example>
+
+    위의 예시와 같이 답변하세요.
+    image_ref 를 [ ] 로 표시하여 예시와 같이 답변하세요
+    예: [공통UI_003] 위 이미지에서 PC 로고를 통해서 PC 연결 상태를 알 수 있습니다.
+    ''',
     input_variables=["question"]
 )
 
-def format_docs(docs):
-    return '\n\n'.join([d.page_content for d in docs])
+final_prompt = '''
+{context}
 
-template = '''
-<|start_header_id|>
-{few_shot_examples}
+질문에 대해 다음 지침을 따라 답변해 주세요:
+1. 간결하고 정확한 답변을 제공해 주세요.
+2. 컨텍스트에 제공된 정보만을 사용해 주세요.
+3. [공통UI_XXX]와 같은 이미지 참조를 포함하여 설명해 주세요.
+4. 불필요한 정보는 제외하고 질문과 직접적으로 관련된 내용만 답변해 주세요.
+5. 확실하지 않은 정보에 대해서는 추측하지 마세요.
 
-간결하고 짧은 답변을 제공하세요. 중복되지 않게 답변하세요.
-<|end_header_id|>
-
-<s>{question}
+질문: {question}
+답변:
 '''
 
-prompt = ChatPromptTemplate.from_template(template)
+prompt = ChatPromptTemplate.from_template(final_prompt)
 
 rag_chain = (
     {
@@ -48,4 +57,4 @@ rag_chain = (
     | StrOutputParser()
 )
 
-print(rag_chain.invoke("Vision S/W 의 상단 공통 UI 에 대해 설명해줘"))
+print(rag_chain.invoke("카메라의 연결상태를 확인하는 방법을 알려줘."))
